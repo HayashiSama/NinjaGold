c738c570060d029aa90fcd6dec432fc092a055dc
Peter Lin <ptrlin@ucdavis.edu>
2018-02-08 16:05:46 -0800

    ninja gold initial commit

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+app = Flask(__name__)
+
+app.secret_key = '01189998819991197253'
+
+@app.route('/')
+def index():
+  session['money']=0
+  session['activity']=""
+  return render_template("index.html")
+
+@app.route('/process_money', methods=['POST'])
+def moneys():
+  value= request.form['building']
+
+  if value=='farm':
+    val = random.randint(10, 20)
+    session['money']+= val
+    session['activity'] = "Entered the farm and got " + str(val) + " gold.\n" + session['activity']
+  elif value=='cave':
+    val = random.randint(5, 10)
+    session['money']+= val
+    session['activity'] = "Entered the cave and got " + str(val) + " gold.\n" + session['activity']
+  elif value == 'house':
+    val = random.randint(2, 5)
+    session['money']+= val
+    session['activity'] = "Entered the house and got " + str(val) + " gold.\n" + session['activity']
+  elif value=='casino':
+    val = random.randint(-50, 50)
+    session['money']+= val
+    session['activity'] = "Entered the casino and got " + str(val) + " gold.\n" + session['activity']
+
+  return render_template("index.html", money=session['money'], activity=session['activity'])
+
+app.run(debug=True)
