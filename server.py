a04085e10dd0eeb9721ab6087236fa6876b079d0
Peter Lin <ptrlin@ucdavis.edu>
2018-02-08 16:37:55 -0800

    timestamp

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 import random
+import datetime
+
 app = Flask(__name__)
 
 app.secret_key = '01189998819991197253'
@@ -13,23 +15,23 @@ def index():
 @app.route('/process_money', methods=['POST'])
 def moneys():
   value= request.form['building']
-
+  #Location + value+ time + append previous session log
   if value=='farm':
     val = random.randint(10, 20)
     session['money']+= val
-    session['activity'] = "Entered the farm and got " + str(val) + " gold.\n" + session['activity']
+    session['activity'] = "Entered the farm and got " + str(val) + " gold. "  + str(datetime.datetime.now()) + "\n" + session['activity']
   elif value=='cave':
     val = random.randint(5, 10)
     session['money']+= val
-    session['activity'] = "Entered the cave and got " + str(val) + " gold.\n" + session['activity']
+    session['activity'] = "Entered the cave and got " + str(val) + " gold. "  + str(datetime.datetime.now()) + "\n" + session['activity']
   elif value == 'house':
     val = random.randint(2, 5)
     session['money']+= val
-    session['activity'] = "Entered the house and got " + str(val) + " gold.\n" + session['activity']
+    session['activity'] = "Entered the house and got " + str(val) + " gold. "  + str(datetime.datetime.now()) + "\n" + session['activity']
   elif value=='casino':
     val = random.randint(-50, 50)
     session['money']+= val
-    session['activity'] = "Entered the casino and got " + str(val) + " gold.\n" + session['activity']
+    session['activity'] = "Entered the casino and got " + str(val) + " gold. "  + str(datetime.datetime.now()) + "\n" + session['activity']
 
   return render_template("index.html", money=session['money'], activity=session['activity'])
 
